724d14c91bfece6d7e15fd27de085d20252c3e75
LarissaGiovanaAlaminos <lari.alaminos@gmail.com>
2023-10-30 00:47:32 -0300

    Ajuste Servico Etapa

--- challenge/src/main/java/com/tech/challenge/inscricao/gestaoetapa/service/EtapaService.java
+++ challenge/src/main/java/com/tech/challenge/inscricao/gestaoetapa/service/EtapaService.java
@@ -4,7 +4,7 @@ import com.tech.challenge.inscricao.gestaoetapa.dto.EtapaDTO;
 import com.tech.challenge.inscricao.gestaoetapa.dto.EtapaRequestDTO;
 import com.tech.challenge.inscricao.gestaoetapa.entity.Etapa;
 import com.tech.challenge.inscricao.gestaoetapa.repository.EtapaRepository;
-import com.tech.challenge.inscricao.gestaousuario.controller.exception.ControllerNotFoundException;
+import com.tech.challenge.acesso.gestaousuario.controller.exception.ControllerNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
