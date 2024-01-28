b925fc2161dda696705e9cdf2f2ba6f39c175c06
Thiago L. Cordeiro <thiagolcordeiro@gmail.com>
2024-01-28 17:27:46 -0300

    Correção no MeioPagamento

--- tech-challenge-fase2/api-pagamento/src/main/java/com/fase2/techchallenge/fiap/pagamento/gestaopagamento/dto/MeioPagamentoDTO.java
+++ tech-challenge-fase2/api-pagamento/src/main/java/com/fase2/techchallenge/fiap/pagamento/gestaopagamento/dto/MeioPagamentoDTO.java
@@ -1,6 +1,6 @@
 package com.fase2.techchallenge.fiap.pagamento.gestaopagamento.dto;
 
-public record SolicitacaoReciboDTO(
+public record MeioPagamentoDTO(
         String id,
         String descricao,
         String situacao
